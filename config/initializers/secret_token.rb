b870f2e854cf55b0eb61b787ae4c0bb2d58e3bf8
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-02-12 16:36:36 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covers::Application.config.secret_token = '736a2136408e51acc25ca2d0f061d7b6779c4371a774fda4be75309f5a137c4b8c18cfc563ee65dc91d94064e2c1ab1ef166ee7c0211cc5265b9ff79520df31d'
