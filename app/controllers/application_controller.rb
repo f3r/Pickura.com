f83f9f8854494c1173d5ce38b5589775518c10d9
Fer Martin <fer@heypal.com>
2012-03-03 01:04:03 +0800

    new homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'main'
+  
+  private
+    before_filter :instantiate_controller_and_action_names
+
+    def instantiate_controller_and_action_names
+        @current_action = action_name
+        @current_controller = controller_name
+    end
 end
