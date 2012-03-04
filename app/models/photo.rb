5b0c2159ea05386398d8b0f2242b3347e0860dfa
Fer Martin <fer@heypal.com>
2012-03-04 15:25:02 +0800

    Removing cache from ActiveRecord

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,21 +16,15 @@ class Photo < ActiveRecord::Base
   def self.search(page=1, per_page=20, tags=nil)
     # Returns a collection of photos (with memcached)
     if tags.nil?
-      Rails.cache.fetch("photos__#{page}") {
-        Photo.paginate(:page => page, :per_page => per_page).order('id ASC')
-      }
+      Photo.paginate(:page => page, :per_page => per_page).order('id ASC')
     else
-      Rails.cache.fetch("photosTags__#{tags}__#{page}") {
-        Photo.tagged_with(tags).paginate(:page => page, :per_page => per_page).order('id ASC')
-      }
+      Photo.tagged_with(tags).paginate(:page => page, :per_page => per_page).order('id ASC')
     end
   end
 
   # Returns a collection of tags (with memcached)
   def self.relevant_tags(order = 'count desc', at_least = '10')
-    Rails.cache.fetch("tagcount__#{order}__#{at_least}") {
-      Photo.tag_counts(:order => order, :at_least => at_least)
-    }
+    Photo.tag_counts(:order => order, :at_least => at_least)
   end
 
   def move_watermark(corner = nil)
