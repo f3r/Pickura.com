06f4711dcd33997f787c6c58c0fe2298f45050f0
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-03-04 15:21:12 +0800

    Changing flow

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Covers::Application.routes.draw do
   match 'cover/:id/download'  => 'editor#download',     :as => :download_cover
   match 'cover/:id/upload'    => 'editor#upload',       :as => :upload_cover
   match 'editor/:id'          => 'editor#show',         :as => :editor
-  #match 's/:id'               => 'photos'
+  #match 's/:id'              => 'photos'
 
   root :to => 'home#index'
 end
