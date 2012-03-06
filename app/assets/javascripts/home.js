0698735d6fdd9eed645e3e02325c3d6ebf6a5d7b
Fer Martin <fer@heypal.com>
2012-03-06 22:10:10 +0800

    images in homepage go to gallery on click, stop auto play on mouseover

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,33 +1,32 @@
-// $(function(){
-//   $('#pics a').click(function() {
-//     $('#user_image').attr("src", $(this).data('target'));
-//   });
-// });
-
-easing_options = ['swing',          
-                  'easeInQuad',     'easeOutQuad',    'easeInOutQuad',
-                  'easeInCubic',    'easeOutCubic',   'easeInOutCubic',
-                  'easeInQuart',    'easeOutQuart',   'easeInOutQuart',
-                  'easeInQuint',    'easeOutQuint',   'easeInOutQuint',
-                  'easeInSine',     'easeOutSine',    'easeInOutSine',
-                  'easeInExpo',     'easeOutExpo',    'easeInOutExpo',
-                  'easeInCirc',     'easeOutCirc',    'easeInOutCirc',
-                  'easeInElastic',  'easeOutElastic', 'easeInOutElastic',
-                  'easeInBack',     'easeOutBack',    'easeInOutBack',
-                  'easeInBounce',   'easeOutBounce',  'easeInOutBounce']
+// from: http://gsgd.co.uk/sandbox/jquery/easing/
+easing_options = ['swing',                                                //  0
+                  'easeInQuad',     'easeOutQuad',    'easeInOutQuad',    //  1  - 2 -  3
+                  'easeInCubic',    'easeOutCubic',   'easeInOutCubic',   //  4  - 5 -  6
+                  'easeInQuart',    'easeOutQuart',   'easeInOutQuart',   //  7  - 8 -  9
+                  'easeInQuint',    'easeOutQuint',   'easeInOutQuint',   // 10 - 11 - 12
+                  'easeInSine',     'easeOutSine',    'easeInOutSine',    // 13 - 14 - 15
+                  'easeInExpo',     'easeOutExpo',    'easeInOutExpo',    // 16 - 17 - 18
+                  'easeInCirc',     'easeOutCirc',    'easeInOutCirc',    // 19 - 20 - 21
+                  'easeInElastic',  'easeOutElastic', 'easeInOutElastic', // 22 - 23 - 24
+                  'easeInBack',     'easeOutBack',    'easeInOutBack',    // 25 - 26 - 27
+                  'easeInBounce',   'easeOutBounce',  'easeInOutBounce']  // 28 - 29 - 30
 
 $(document).ready(function() {
   $('ul.slider').roundabout({
     autoplay : true,
     duration : 1300,
-    autoplayDuration: 4000,
+    autoplayDuration: 3500,
     tilt:      -5,
     minOpacity: 0,
     minScale:   0,
     enableDrag: true,
-    easing: easing_options[2]
-    // maxScale: 1.0,
-    // shape: 'tearDrop'
-    // shape: 'rollerCoaster' 
+    easing:     easing_options[1],
+    dropEasing: easing_options[11],
+    autoplayPauseOnHover: true
   });
+  
+  // $('.slider img').click(function() {
+  //     $('#user_image').attr("src", $(this).data('target'));
+  //   });
+  
 });
