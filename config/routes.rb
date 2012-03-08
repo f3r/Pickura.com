4fb9adea7da0c337ec6b753b940796cd7cd5770c
Jorge Yau <jorgeyau@gmail.com>
2012-03-07 23:17:25 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Covers::Application.routes.draw do
   match 'most-popular'        => 'covers#most_popular',   :as => :gallery_popular
   match 'editors-pick'        => 'covers#editor_pick',    :as => :gallery_editor_pick
   match 'gallery/:id'         => 'covers#show',           :as => :gallery_show, :constraints => lambda{|req| req["id"].include? "facebook-timeline-cover" }
-  match 'gallery/:id'         => 'covers#index',          :as => :gallery, :constraints => lambda{|req| !req["id"].include? "facebook-timeline-cover" }
+  match 'gallery/:id'         => 'covers#index',          :as => :gallery_list, :constraints => lambda{|req| !req["id"].include? "facebook-timeline-cover" }
+  match 'gallery'             => 'covers#index',          :as => :gallery
   match 'cover/:id/download'  => 'editor#download',       :as => :download_cover
   match 'cover/:id/upload'    => 'editor#upload',         :as => :upload_cover
   match 'cover/premium'       => 'covers#premium'
